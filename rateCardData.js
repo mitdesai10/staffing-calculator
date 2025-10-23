cec63d41e310451cc14808eee36c7f53ea020e0f
mitdesai10 <157089066+mitdesai10@users.noreply.github.com>
2025-10-23 11:52:12 -0400

    Multiple roles calculation

---
+++ rateCardData.js
@@ -0,0 +1,94 @@
+// Rate Card Data
+const rateCardData = [
+  {
+    "role": "Salesforce Solution Architect",
+    "clientRate": 190.0,
+    "onshore": {
+      "cost": 100.0
+    },
+    "offshore": {
+      "cost": 34.0
+    },
+    "nearshore": {
+      "cost": 47.0
+    }
+  },
+  {
+    "role": "Marketing Cloud Specialist",
+    "clientRate": 190.0,
+    "onshore": {
+      "cost": 66.0
+    },
+    "offshore": {
+      "cost": 9.0
+    },
+    "nearshore": {
+      "cost": 32.0
+    }
+  },
+  {
+    "role": "Commerce Cloud Administrator",
+    "clientRate": 160.0,
+    "onshore": {
+      "cost": 69.0
+    },
+    "offshore": {
+      "cost": 12.5
+    },
+    "nearshore": {
+      "cost": 34.0
+    }
+  },
+  {
+    "role": "Junior Developer",
+    "clientRate": 140.0,
+    "onshore": {
+      "cost": 69.0
+    },
+    "offshore": {
+      "cost": 11.0
+    },
+    "nearshore": {
+      "cost": 25.0
+    }
+  },
+  {
+    "role": "Release Manager",
+    "clientRate": 160.0,
+    "onshore": {
+      "cost": 99.0
+    },
+    "offshore": {
+      "cost": 13.5
+    },
+    "nearshore": {
+      "cost": 56.0
+    }
+  },
+  {
+    "role": "Data/Integration Architect",
+    "clientRate": 190.0,
+    "onshore": {
+      "cost": 129.0
+    },
+    "offshore": {
+      "cost": 30.0
+    },
+    "nearshore": {
+      "cost": 70.0
+    }
+  },
+  {
+    "role": "QA -Quality Assurance",
+    "clientRate": 18.0,
+    "onshore": {
+      "cost": 0
+    },
+    "offshore": {
+      "cost": 6.75
+    },
+    "nearshore": {
+      "cost": 0
+    }
+  }
+];
